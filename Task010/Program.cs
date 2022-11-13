fe29027f2aaca50dd82e1a1cde0cb62e3935c684
IrinaPenkina <nikolay_irina@mail.ru>
2022-11-14 00:35:47 +0300

    Task010 done

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int size = number / 2;
+int count = 0;
+
+if (number > 0)
+{
+    while (count < size)
+    {
+        int output = count * 2 + 2;
+        Console.Write($"{output} ");
+        count++;
+    }
+}
+else Console.WriteLine("Введено неверное число");
