fee3aa5027edcfa18bdbca6b1071798d7c4cf30a
IrinaPenkina <nikolay_irina@mail.ru>
2022-11-19 23:07:36 +0300

    task019 added

---
+++ Task019/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций 
+// (целочисленное деление, остаток от деления).
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int numberFiveDigit = 9999;
+while (numberFiveDigit < 10000 || numberFiveDigit > 99999)
+{
+    Console.WriteLine("Введите натуральное пятизначное число");
+    numberFiveDigit = Convert.ToInt32(Console.ReadLine());
+    if (numberFiveDigit < 10000 || numberFiveDigit > 99999) Console.WriteLine("Введено некорректное число");
+}
+
+string PalindromeCheck(int num)
+{
+    int firstDigit = num % 10;
+    num = num / 10;
+    int secondDigit = num % 10;
+    num = num / 10;
+    int thirdDigit = num % 10;
+    num = num / 10;
+    int fourthDigit = num % 10;
+    num = num / 10;
+    int fifthDigit = num % 10;
+    // Console.WriteLine($"{firstDigit}, {secondDigit}, {thirdDigit}, {fourthDigit}, {fifthDigit}");
+    return (fifthDigit == firstDigit && fourthDigit == secondDigit)
+        ? "Да" : "Нет";
+}
+
+Console.WriteLine(PalindromeCheck(numberFiveDigit));
