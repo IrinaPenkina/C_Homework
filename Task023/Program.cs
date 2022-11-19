9445c8f5f2cb0b1ae2222b0cfe6357e1970ba41a
IrinaPenkina <nikolay_irina@mail.ru>
2022-11-19 23:31:52 +0300

    task023 done

--- Task023/Program.cs
+++ Task023/Program.cs
@@ -6,3 +6,26 @@
 // 3 | 27
 // 4 | 64
 // 5 | 125
+
+int number = -1;
+while (number <= 0)
+{
+    Console.WriteLine("Введите натуральное число");
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number <= 0) Console.WriteLine("Введено некорректное число");
+}
+
+string Table (int num)
+{
+    int count = 1;
+    string table = "";
+    while (count <= num)
+    {
+        table = table + $"{count}|{count * count * count} \n";
+        count ++;
+    }
+    return table;
+}
+
+string table = Table(number);
+Console.WriteLine(table);
