a360a6b820143cecce44c02314241a91917b4bfa
IrinaPenkina <nikolay_irina@mail.ru>
2022-12-11 21:33:27 +0300

    task064, task066, task068

---
+++ Task066/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers (int num1, int num2)
+{
+    if (num2 == num1) return num2;
+    return num1 < num2 ? num2 + SumNumbers(num1, num2 - 1): num1 + SumNumbers(num1 - 1, num2);
+}
+
+Console.Write("Задайте первое натуральное число: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте второе натуральное число: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumNumbers(numM, numN);
+Console.WriteLine($"Сумма чисел от {numM} до {numN} равна {sum}");
+
---
+++ Task068/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Akk (int numM, int numN)
+{
+    if (numM == 0) return numN + 1;
+    else if (numM != 0 && numN == 0) return Akk(numM - 1, 1);
+    return Akk(numM - 1, Akk(numM, numN - 1));
+}
+
+Console.Write("Задайте первое неотрицательное целое число: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте второе неотрицательное целое число: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int sum = Akk(numM, numN);
+Console.WriteLine($"Функция Аккермана от аргументов ({numM}, {numN}) равна {sum}");
